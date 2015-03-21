350160d2857b01ef00e709c45125c52fc20dbb7d
Robert M Flight <r.flight79@gmail.com>
2015-03-21 16:07:44 -0400

    added routine to check if the indices in the gg_heatmap function were character or match the number of rows in the original data so that we don't get odd plot behavior

--- R/visqc.R
+++ R/visqc.R
@@ -18,8 +18,8 @@ summarize_data <- function(in_data, sample_classes=NULL, avg_function = mean){
   split_indices <- split(seq(1, nrow(in_data)), sample_classes)
   
   split_values <- lapply(split_indices, function(use_index){
-    tmp_mean <- apply(in_data[use_index, ], 2, avg_function)
-    tmp_sd <- apply(in_data[use_index, ], 2, sd)
+    tmp_mean <- apply(in_data[use_index, , drop = FALSE], 2, avg_function)
+    tmp_sd <- apply(in_data[use_index, , drop = FALSE], 2, sd)
     data.frame(mean = tmp_mean, var = c(tmp_sd, tmp_sd / tmp_mean), type = rep(c("sd", "rsd"), each = n_feature))
   })
   
@@ -48,7 +48,20 @@ gg_heatmap <- function(dataMatrix, cols = NULL, limits = NULL){
     cols = grey.colors(10, start = 0, end = 1)
   }
   
+  nEntry <- nrow(dataMatrix)
+  dataIndices <- seq(1, nEntry)
+  
   meltedData <- melt(dataMatrix)
+  
+  # checking if we have numeric values as indices, do they correspond to the
+  # actual indices, and if not, change them to character
+  if (is.integer(meltedData$Var1)){
+    if (sum(dataIndices %in% meltedData$Var1) != nEntry){
+      meltedData$Var1 <- as.character(meltedData$Var1)
+      meltedData$Var2 <- as.character(meltedData$Var2)
+    }
+  }
+  
   outPlot <- ggplot(meltedData, aes(x = Var1, y = Var2, fill = value)) + geom_tile()
   outPlot <- outPlot + scale_fill_gradientn(colours = cols, limits = limits)
   # outPlot <- outPlot + scale_x_continuous(expand = c(0, 0)) + 
