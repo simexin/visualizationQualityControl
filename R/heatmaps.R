bc4ae950468231f503be7ed9933f8c6c2e7bc223
Robert M Flight <r.flight79@gmail.com>
2015-06-23 10:56:03 -0400

    added the generate_heatmap function to make pretty annotated heatmaps

--- R/heatmaps.R
+++ R/heatmaps.R
@@ -66,3 +66,63 @@ similarity_reorderbyclass <- function(similarity_matrix, sample_classes, transfo
   
   out_order <- unlist(new_order)
 }
+
+
+#' easier heatmaps
+#' 
+#' rolls some of the common \code{Heatmap} options into a single function call
+#' to make life easier when creating lots of heatmaps. \strong{Note:} this function
+#' does \emph{reorder} the rows or columns of the matrix before visualization,
+#' the order going in is what will be presented.
+#' 
+#' @param matrix_data the matrix you want to plot as a heatmap
+#' @param color_values the color mapping of values to colors (see Details)
+#' @param row_color_data data for row annotations
+#' @param row_color_list list for row annotations
+#' @param col_color_data data for column annotations
+#' @param col_color_list list for column annotations
+#' 
+#' @details This function uses the \code{ComplexHeatmap} package to produce
+#' heatmaps with complex row- and column-color annotations. Both \code{row_color_data}
+#' and \code{col_color_data} should be \code{data.frame}'s where each column describes
+#' meta-data about the rows or columns of the matrix. The \code{row_color_list} and 
+#' \code{col_color_list} provide the mapping of color to annotation, where each
+#' \code{list} entry should be a named vector of colors, with the list entry
+#' corresponding to a column entry in the data.frame, and the names of the colors
+#' corresponding to annotations in that column.
+#' 
+#' @examples  
+#' \dontrun{
+#' library(circlize)
+#' colormap <- colorRamp2(c(-1, 0, 1), c("blue", "white", "red"))
+#' mat <- matrix(rnorm(100, sd = 0.5), 10, 10)
+#' rownames(mat) <- colnames(mat) <- letters[1:10]
+#' 
+#' meta_data <- data.frame(grp = rep(c("grp1", "grp2"), each = 5),
+#'                         set = c(rep("set1", 3), rep("set2", 7)))
+#' annotation_color <- c(grp1 = "green", grp2 = "red", set1 = "blue",
+#'                       set2 = "yellow")
+#' 
+#' row_data <- meta_data[, "grp", drop = FALSE]
+#' col_data <- meta_data[, "set", drop = FALSE]
+#' row_annotation = list(grp = annotation_color[1:2])
+#' col_annotation = list(set = annotation_color[3:4])
+#' 
+#' generate_heatmap(mat, colormap, row_color_data = row_data, row_color_list = row_annotation,
+#'                  col_color_data = col_data, col_color_list = col_annotation)
+#' }
+#' 
+#' @import ComplexHeatmap
+#' @export
+generate_heatmap <- function(matrix_data, color_values, title = "", row_color_data = NULL, row_color_list, col_color_data = NULL, col_color_list){
+  if (!is.null(row_color_data)){
+    row_annot <- rowAnnotation(df = row_color_data, col = row_color_list)
+  }
+  if (!is.null(col_color_data)){
+    col_annot <- HeatmapAnnotation(df = col_color_data, col = col_color_list)
+  }
+  
+  heat_out <- Heatmap(matrix_data, col = color_values, cluster_rows = FALSE, cluster_columns = FALSE,
+                      top_annotation = col_annot, column_title = title) + row_annot
+  heat_out
+}
