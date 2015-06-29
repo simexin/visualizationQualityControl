16ff87d5eb7f026de65d4bb05752fb01768ae429
Robert M Flight <r.flight79@gmail.com>
2015-06-29 10:50:11 -0400

    create a factor out of all of the columns of a data.frame

--- R/principal_components.R
+++ R/principal_components.R
@@ -5,20 +5,22 @@
 #' 
 #' @param pca_decomp the results of \code{prcomp}
 #' @param princomps which principal components to plot
-#' @param obj_class factor defining the classes of each object
+#' @param groups factor defining the classes of each object
 #' @param dot_size size of the points (default is 1)
 #'
 #' @import ggbiplot
 #' @import ggplot2
 #' @export
-visqc_pca <- function(pca_decomp, princomps = c(1, 2), obj_class = NULL, dot_size = 1){
+visqc_pca <- function(pca_decomp, princomps = c(1, 2), groups = NULL, dot_size = 1){
   x_data <- pca_decomp$x
   n_obj <- nrow(x_data)
   
-  if (is.null(obj_class)){
-    obj_class <- data.frame(groups = as.factor(rep("", n_obj)))
+  if (is.null(groups)){
+    groups <- as.factor(rep("", n_obj))
+  } else if (is.data.frame(groups)){
+    groups <- as.factor(paste(groups[, ], sep = "."))
   }
   
   
-  ggbiplot(pca_decomp, obs.scale = 1, groups = obj_class$groups, var.axes = FALSE, choices = princomps) + geom_point(aes(color = groups), size = dot_size)
+  ggbiplot(pca_decomp, obs.scale = 1, groups = groups, var.axes = FALSE, choices = princomps) + geom_point(aes(color = groups), size = dot_size) + coord_cartesian()
 }
