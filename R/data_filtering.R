9026eea7df745e5b8aad14241c6308d2d91bf767
Robert M Flight <r.flight79@gmail.com>
2015-07-30 10:58:47 -0400

    modified to use integers as a direct cutoff

--- R/data_filtering.R
+++ R/data_filtering.R
@@ -1,21 +1,33 @@
 #' find non-zero by percentage
 #' 
-#' Given a value matrix (features are rows, samples are columns), and sample classes, 
+#' Given a value matrix (features are columns, samples are rows), and sample classes, 
 #' find those things that are not zero in at least a certain 
 #' number of one of the classes
 #' 
 #' @param data_matrix the matrix of values to work with 
 #' @param sample_classes the classes of each sample
-#' @param keep_perc what percentage of samples in each class need a non-zero value
+#' @param keep_num what number of samples in each class need a non-zero value (see Details)
+#' 
+#' @details The number of samples that must be non-zero can be expressed either as a whole
+#'   number (that is greater than one), or as a fraction that will be be multiplied 
+#'   by the number of samples in each class to get the lower limits for each of the classes.
 #' 
 #' @export
 #' @return matrix
-filter_non_zero_percentage <- function(data_matrix, sample_classes, keep_perc = 0.75){
+filter_non_zero_percentage <- function(data_matrix, sample_classes = NULL, keep_num = 0.75){
+  if (is.null(sample_classes)) {
+    sample_classes <- rep("A", nrow(data_matrix))
+  }
   uniq_classes <- unique(sample_classes)
   class_index <- lapply(uniq_classes, function(x){sample_classes %in% x})
   names(class_index) <- uniq_classes
   
-  min_notzero <- sapply(class_index, function(x){round(sum(x) * keep_perc)})
+  if (keep_num <= 1) {
+    min_notzero <- sapply(class_index, function(x){round(sum(x) * keep_perc)})
+  } else {
+    min_notzero <- sapply(class_index, function(x){round(keep_num)})
+  }
+  
   
   has_min <- apply(data_matrix, 2, function(in_col){
     n_pass <- sapply(class_index, function(index){sum(in_col[index] > 0)})
